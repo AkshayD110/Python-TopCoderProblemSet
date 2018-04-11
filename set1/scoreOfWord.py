04b2d44aa2701a570316b2b4e9d2e9a9850122e2
AkshayD110 <akshayd110@gmail.com>
2018-04-12 00:59:59 +0530

    function to calculate score implemented

--- set1/scoreOfWord.py
+++ set1/scoreOfWord.py
@@ -1,6 +1,16 @@
-
-word=input("Enter the input word [all upper case]:")
+from collections import Counter
 
 def score_calculation(word):
+    word_cntr=Counter(list(word))
+    score_list=list(word_cntr.values())
+    total_score=sum([(x*x) for x in score_list]) #list comprehension to square each number and sum it.
+    print(total_score)
+
+def best_change(word):
+    '''Implement the logic to change the letter to get best score'''
     pass
+
+
+word=input("Enter the input word [all upper case]:")
+score_calculation(word)
 
