990e6e6d27a64fcff23102d2aa4419450e50ce35
AkshayD110 <akshayd110@gmail.com>
2018-04-12 15:49:58 +0530

    check for input validations

--- set1/scoreOfWord.py
+++ set1/scoreOfWord.py
@@ -1,13 +1,20 @@
 from collections import Counter
 
 def score_calculation(word):
+    '''Below two conditiopns to check inputs validations'''
+    if (len(word)>50):
+        raise ValueError("The length of input word should be with in 50characters")
+    if (word.islower()):
+        raise ValueError("The word should be in upper case")
     word_cntr=Counter(list(word))
+    print(word_cntr)
     score_list=list(word_cntr.values())
     total_score=sum([(x*x) for x in score_list]) #list comprehension to square each number and sum it.
     print(total_score)
 
 def best_change(word):
     '''Implement the logic to change the letter to get best score'''
+
     pass
 
 
