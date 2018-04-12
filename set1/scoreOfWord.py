e1e1f1b9edfacb9b97379d18375acf301ed55a0f
AkshayD110 <akshayd110@gmail.com>
2018-04-12 19:09:40 +0530

    all logic works -- Completed.

--- set1/scoreOfWord.py
+++ set1/scoreOfWord.py
@@ -1,21 +1,28 @@
 from collections import Counter
 
 def score_calculation(word):
-    '''Below two conditiopns to check inputs validations'''
+    '''Below two conditions to check inputs validations'''
     if (len(word)>50):
         raise ValueError("The length of input word should be with in 50characters")
-    if (word.islower()):
+    #Below 2 lines to check the input word contains smallcase character
+    if not (word.isupper()):
         raise ValueError("The word should be in upper case")
+    #below section for replacing second most occuring char with first
     word_cntr=Counter(list(word))
+    two_common_characters=word_cntr.most_common(2)
+    first_common=two_common_characters[0]
+    first_char=first_common[0]
+    second_common=two_common_characters[1]
+    second_char=second_common[0]
+
+    #below section for score calculation
+    word=word.replace(second_char, first_char)
+    word_cntr = Counter(list(word))
     print(word_cntr)
     score_list=list(word_cntr.values())
     total_score=sum([(x*x) for x in score_list]) #list comprehension to square each number and sum it.
     print(total_score)
 
-def best_change(word):
-    '''Implement the logic to change the letter to get best score'''
-
-    pass
 
 
 word=input("Enter the input word [all upper case]:")
