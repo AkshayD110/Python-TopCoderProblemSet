9874385a0602188126948b773844b40cc8d69e5f
AkshayD110 <akshayd110@gmail.com>
2018-04-13 21:56:02 +0530

    monitoring connections from RDC box to --DB --OAC compute

---
+++ set2/mainclass.py
@@ -0,0 +1,10 @@
+from set2 import iterationScore
+
+def main():
+    word1 = input("Enter your first word")
+    word2 = input("Enter your second word")
+    word1 = word1.upper()
+    word2 = word2.upper()
+
+if __name__ == '__main__':
+    main()
