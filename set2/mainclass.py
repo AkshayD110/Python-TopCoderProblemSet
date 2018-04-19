8f60dc6c227a77e24aa331d5da4553263a0f9378
AkshayD110 <akshayd110@gmail.com>
2018-04-19 14:21:47 +0530

    using assert for conditions

--- set2/mainclass.py
+++ set2/mainclass.py
@@ -6,8 +6,9 @@ def main():
     word1 = word1.upper()
     word2 = word2.upper()
     #below two conditions check the input condition
-    if (len(word1) != len(word2)):
-        raise ValueError("Both the words need to be of same lenght")
+    assert len(word1)==len(word2), "Both the words should be of same length"
+    #if (len(word1) != len(word2)):
+     #   raise ValueError("Both the words need to be of same lenght")
     if (len(word1) and len(word2) > 50):
         raise ValueError(f"Length of {word1} and {word2} should be less than 50 characters")
     get_score_obj = iterationScore.iterationScore(word1, word2)
